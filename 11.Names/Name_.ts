ee6ca32dc6fd53dcfc7b23b14d94187de526a2c4
ayesha-sikander <159716045+ayesha-sikander@users.noreply.github.com>
2024-03-27 00:38:47 +0500

    Delete 11.Names directory

--- 11.Names/Name_.ts
+++
@@ -1,7 +0,0 @@
-// Define an array called names and store the names of your friends.
-const names: string[] = ['Ayesha','Habiba','Anaya','Sana'];
-
-// Print each person's name by accessing each element in the list, one at a time.
-for (let i = 0; i < names.length; i++) {
-    console.log(names[i]);
-}
